491933c82f76ce506bdd0c6bd49fb1f18d6a9723
Saksham Thapar <sakshamthapar22@gmail.com>
2026-03-12 10:06:35 +0530

    Use Case 1 implemented - Application entry and welcome message

---
+++ src/com/bookmystay/HotelBookingApp.java
@@ -0,0 +1,41 @@
+package com.bookmystay;
+
+/**
+ * ======================================================
+ * MAIN CLASS - HotelBookingApp
+ * ======================================================
+ *
+ * Use Case 1: Application Entry & Welcome Message
+ *
+ * Description:
+ * This class represents the entry point of the
+ * Hotel Booking Management System.
+ *
+ * At this stage, the application:
+ * - Starts execution from the main() method
+ * - Displays a welcome message to the user
+ * - Confirms that the system has started successfully
+ *
+ * No business logic is implemented yet.
+ *
+ * @author Developer
+ * @version 1.0
+ */
+
+public class HotelBookingApp {
+
+    /**
+     * Application entry point
+     * @param args command line arguments
+     */
+    public static void main(String[] args) {
+
+        System.out.println("=======================================");
+        System.out.println("Welcome to the Hotel Booking Management System");
+        System.out.println("Application: Book My Stay App");
+        System.out.println("Version: 1.0");
+        System.out.println("System started successfully!");
+        System.out.println("=======================================");
+
+    }
+}
